908bf1634ec07eac4f2ed76ca4bf91ed09feea56
Justin Gosses <jgosses82@gmail.com>
2019-05-11 22:23:15 -0500

    fixed some small print issues in predictionclasses_runner.py

--- predictatops/predictionclasses_runner_old.py
+++ predictatops/predictionclasses_runner_old.py
@@ -10,15 +10,20 @@ from main import getJobLibPickleResults
 # model = getJobLibPickleResults(output_data_inst,output_data_inst.path_trainclasses,"trainclasses_model.pkl")
 # ML1 = getJobLibPickleResults(output_data_inst,output_data_inst.path_trainclasses,"trainclasses_ML1_instance.pkl")
 
-model,ML1 =loadMLinstanceAndModel(output_data_inst)
+model,ML1 =loadMLinstanceAndModel()
 
-print("the model imported is:",model)
+vs = {
+    "depth_str" = config.DEPTH_col_in_featureCreation,
+"pick_class_str" = config.pick_class_str,
+"UWI_str" = config.UWI,
+"rollingWindows" = config.curve_windows_,
+"distClassIntegersArray" = list(config.zonesAroundTops.keys())
+}
 
 ################## Class Prediction Results for training dataframe for X #############
 
 ##### Creating a class_accuracy instance with the already established ML1 variable for an isntance of the ML_obj_class
 ac = class_accuracy(ML1)
-
 
 ################## First with training data #############
 
@@ -27,7 +32,6 @@ ac = class_accuracy(ML1)
 accuracy_train = ac.run_all(model,ac.train_X,ac.train_y,'TopTarget_Pick_pred','class_DistFrPick_TopTarget')
 
 print("accuracy of training dataset",accuracy_train)
-
 
 ################## Then with test data ###############
 
@@ -52,7 +56,7 @@ print("accuracy of test dataset",accuracy_test)
 #     Sums of rolling all means
 
 
-concatClass = InputDistClassPrediction_to_BestDepthForTop(output_data_inst)
+concatClass = InputDistClassPrediction_to_BestDepthForTop()
 
 concatClass.load_MLobj(ML1)
 
@@ -60,14 +64,15 @@ concatClass.help()
 
 prediction_distClass_trainingData_ndarray = concatClass.predict_from_model(model,ML1.train_X)
 
-concatClass1 = concatClass.concat_modelResultsNDArray_w_indexValues(concatClass.result_df_dist_class_prediction,"train",config.pick_class_str)
-
+concatClass1 = concatClass.concat_modelResultsNDArray_w_indexValues(concatClass.result_df_dist_class_prediction,"train",concatClass1 = concatClass.concat_modelResultsNDArray_w_indexValues(concatClass.result_df_dist_class_prediction,"train",config.pick_class_str)
+)
 
 ##### NEED TO PUT THIS LIST IN CONFIG ########## !!!!!!!
 cols_to_keep_list = ['DEPT',"NN1_TopHelper_DEPTH","NN1_thickness","topTarget_Depth_predBy_NN1thick","DistFrom_NN1ThickPredTopDepth_toRowDept"]
 
 concatClass2 = concatClass.concat_step2(ML1,"train",cols_to_keep_list)
 
+#distClassDF_wRollingCols_training = concatClass.run_all(concatClass.df_results_trainOrtest_wIndex,vs.depth_str,vs.pick_class_str,vs.UWI_str,vs.rollingWindows,vs.distClassIntegersArray)
 #####
 DEPTH_col_in_featureCreation = config.DEPTH_col_in_featureCreation
 pick_class_str = config.pick_class_str
@@ -80,24 +85,17 @@ distClassDF_wRollingCols_training = concatClass.calc_pred_vs_real_top_dif(concat
 
 print("distClassDF_wRollingCols_training.head() = ",distClassDF_wRollingCols_training.head())
 
-print("printing distClassDF_wRollingCols_training for checking that it makes sense::",distClassDF_wRollingCols_training.tail())
+####### or run the full thing at once
+#  distClassDF_wRollingCols_training2 = concatClass.run_all(ML1,model,"train",vs,cols_to_keep_list,concatClass.df_results_trainOrtest_wIndex,vs.depth_str,vs.pick_class_str,vs.UWI_str,vs.rollingWindows,vs.distClassIntegersArray)
+
+#print("distClassDF_wRollingCols_training2.head()",distClassDF_wRollingCols_training2.head())
 
 ################ Now lets run test version   ################
 
-concatClass_test = InputDistClassPrediction_to_BestDepthForTop(output_data_inst)
-
-
-#####
-DEPTH_col_in_featureCreation = config.DEPTH_col_in_featureCreation
-pick_class_str = config.pick_class_str
-UWI = config.UWI
-curve_windows_for_rolling_features = config.curve_windows_for_rolling_features
-label_intergers = list(config.zonesAroundTops.keys())
-
-
+concatClass_test = InputDistClassPrediction_to_BestDepthForTop()
 
 #### Doing it the 'all at once' way this time.
-distClassDF_wRollingCols_testData = concatClass_test.run_all(ML1,model,"test",cols_to_keep_list,DEPTH_col_in_featureCreation,pick_class_str,UWI,curve_windows_for_rolling_features,label_intergers)
+distClassDF_wRollingCols_testData = concatClass_test.run_all(ML1,model,"test",vs,cols_to_keep_list,vs.depth_str,vs.pick_class_str,vs.UWI_str,vs.rollingWindows,vs.distClassIntegersArray)
 
 print("distClassDF_wRollingCols_testData.head()",distClassDF_wRollingCols_testData.head())
 
@@ -107,49 +105,51 @@ print("distClassDF_wRollingCols_testData.head()",distClassDF_wRollingCols_testDa
 ######################  known pick based on rolling average & median ranking of depths with distance class #####################  
 #####################  predictions of being close to pick. #####################  
 
-vs = {"depth_str":config.DEPTH_col_in_featureCreation,"pick_class_str":config.pick_class_str,"UWI_str" :config.UWI,"rollingWindows":config.curve_windows_for_rolling_features,"distClassIntegersArray" :list(config.zonesAroundTops.keys())}
-print("vs",vs)
-print("gap")
-print("vs",vs["depth_str"])
 
-
-#####################  Start accuracy_singleTopPerWellPrediction_fromRollingRules() class for training data  #####################  
+### Start accuracy_singleTopPerWellPrediction_fromRollingRules() class for training data ####
 
 rollToWell = accuracy_singleTopPerWellPrediction_fromRollingRules(ML1,vs,distClassDF_wRollingCols_training)
-
-r2,mean_absolute_error_,df_calc_pred_Top_Pick_pred_DEPT_pred = rollToWell.run_all('TopTarget_Pick_pred_DEPT_pred','TopTarget_DEPTH',keepAllWells="yes",dropIfOnlyClasses=[0])
-
+r2,mean_absolute_error_,df_calc_pred_Top_Pick_pred_DEPT_pred = rollToWell.run_all('TopTarget_Pick_pred_DEPT_pred','TopTarget_DEPTH')
 
 print("len(df_calc_pred_Top_Pick_pred_DEPT_pred",len(df_calc_pred_Top_Pick_pred_DEPT_pred))
 print("r2 of training dataset in terms of pick depths = ",r2)
 print("mean_absolute_error_ of training dataset in terms of pick depths = ",mean_absolute_error_)
 
-print("percent of wells kept because they weren't just class zero in rollToWell function for training:",rollToWell.precentWellsKept)
+print("len(train_list)",len(train_list))
 
-#####################  Now accuracy for test dataset again via accuracy_singleTopPerWellPrediction_fromRollingRules()  #####################  
+test_list_short = []
+for item in test_list:
+    if item > 3:
+        test_list_short.append(item)
+len(test_list_short)
 
-#####################  First we'll do it and keep all wells not matter whether or not they include any class predictions other than zero  #####################  
-#####################  This will include some wells with some very bad predictions as there are not class predictions to go off of #####################  
-
+############### Now TEST data version ###############
 
 rollToWell_test = accuracy_singleTopPerWellPrediction_fromRollingRules(ML1,vs,distClassDF_wRollingCols_testData)
-r2_test,mean_absolute_error_test,df_calc_pred_Top_Pick_pred_DEPT_pred2 = rollToWell_test.run_all('TopTarget_Pick_pred_DEPT_pred','TopTarget_DEPTH',keepAllWells="yes",dropIfOnlyClasses=[0])
+r2_testData,mean_absolute_error_testData,df_calc_pred_Top_Pick_pred_DEPT_pred_testData = rollToWell_test.run_all('TopTarget_Pick_pred_DEPT_pred','TopTarget_DEPTH')
 
-print("r2 for test and all wells kept is:",r2_test)
-print("mean_absolute_error_test for test and all wells kept is:",mean_absolute_error_test)
-print("percent wells kept for test and all wells kept is:",rollToWell_test.precentWellsKept)
 
-#####################  Now let's try it for test dataset but exclude wells that only have a zero class prediction #####################  
+print("r2 of test dataset in terms of pick depths = ",r2_testData)
+print("mean_absolute_error_ of test dataset in terms of pick depths = ",mean_absolute_error_testData)
 
-rollToWell_test_NoZeros = accuracy_singleTopPerWellPrediction_fromRollingRules(ML1,vs,distClassDF_wRollingCols_testData)
-r2_test_NoZeros ,mean_absolute_error_test_NoZeros ,df_calc_pred_Top_Pick_pred_DEPT_pred2_NoZeros  = rollToWell_test_NoZeros .run_all('TopTarget_Pick_pred_DEPT_pred','TopTarget_DEPTH',keepAllWells="no",dropIfOnlyClasses=[0])
+############### The prediction are off in large part because a few wells have no ############### 
+################ prediction at all, which is treated as a zero! In real life, we would happily ############### 
+################  know we can't predict those wells with this model (by virtue of getting a zero ############### 
+################  for them) and ignore them, so let's use that in our stats too! ############### 
 
-print("r2 for test and wells excluded that only had zero class predicted is:",r2_test_NoZeros)
-print("mean_absolute_error_test for test and wells excluded that only had zero class predicted is:",mean_absolute_error_test_NoZeros)
-print("percent wells kept for test and wells excluded that only had zero class predicted is:",rollToWell_test_NoZeros.precentWellsKept)
+print("next bit is This is accuracy of prediction for each well of test dataset but excludes the few wells that only have zeros predicted for distance class at every point!!")
 
-number_of_wells_thrown_out = len(rollToWell_test_NoZeros.fullUWIsSet) - len(rollToWell_test_NoZeros.fullUWIsSet)*rollToWell_test_NoZeros.precentWellsKept
+rollToWell_test_noZeros = accuracy_singleTopPerWellPrediction_fromRollingRules(ML1,vs,distClassDF_wRollingCols_testData)
 
-print("number of wells with only zeros predicted that were thrown out:",number_of_wells_thrown_out)
+r2_testData_NoZeros,mean_absolute_error_testData_NoZeros,df_calc_pred_Top_Pick_pred_DEPT_pred_testData_NoZeros = rollToWell_test_noZeros.run_all('TopTarget_Pick_pred_DEPT_pred','TopTarget_DEPTH',keepAllWells="yes",dropIfOnlyClasses=[0])
+
+r2_testData_NoZeros
+
+print("r2 of test dataset without few wells we know will be incorrect without seeing answer in terms of pick depths = ",r2_testData_NoZeros)
+print("mean_absolute_error_ r2 of test dataset without few wells we know will be incorrect without seeing answer in terms of pick depths = ",mean_absolute_error_testData_NoZeros)
+print("The percent of the wells kept is : ",rollToWell_test_noZeros.precentWellsKept)
+
+
+
 
 
