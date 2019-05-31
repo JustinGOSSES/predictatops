486cdc73ca216440c6d88dc7832144758fc44a09
Justin Gosses <jgosses82@gmail.com>
2019-05-30 23:07:25 -0500

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -33,8 +33,8 @@ import predictatops
 # needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode','recommonmark']
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones. extensions = ['sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode','recommonmark','sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
